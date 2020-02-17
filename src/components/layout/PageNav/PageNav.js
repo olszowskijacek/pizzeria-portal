db28c94e49bd2838849c98f7fb894e84e2d3cb1f
Jacek Olszowski <jacek.olszowski1@gmail.com>
2020-02-17 15:29:09 +0100

    Material UI - PageNav

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -1,32 +1,34 @@
 import React from 'react';
+import styles from './PageNav.module.scss';
 import { NavLink } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 const PageNav = () => (
-  <nav>
-    <NavLink exact to={`${process.env.PUBLIC_URL}/`}
+  <nav className={styles.component}>
+    <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/`}
       activeClassName="active">
       Dashboard
-    </NavLink>
+    </Button>
 
-    <NavLink exact to={`${process.env.PUBLIC_URL}/login`}
+    <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/login`}
       activeClassName="active">
       Login
-    </NavLink>
+    </Button>
 
-    <NavLink exact to={`${process.env.PUBLIC_URL}/tables`}
+    <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/tables`}
       activeClassName="active">
       Tables
-    </NavLink>
+    </Button>
 
-    <NavLink exact to={`${process.env.PUBLIC_URL}/waiter`}
+    <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/waiter`}
       activeClassName="active">
       Waiter
-    </NavLink>
+    </Button>
 
-    <NavLink exact to={`${process.env.PUBLIC_URL}/kitchen`}
+    <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/kitchen`}
       activeClassName="active">
       Kitchen
-    </NavLink>
+    </Button>
   </nav>
 );
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './styles/global.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
